04ba106bbd49a429b47178767c17ba310d0f1cd2
SiewLeng <siewleng.lim@gmail.com>
2023-02-26 20:59:46 +0800

    timed-online-assignment/JUL_2022/question_2_a.py

--- ICT113/timed-online-assignment/JUL_2022/question_2.py
+++ ICT113/timed-online-assignment/JUL_2022/question_2.py
@@ -51,11 +51,12 @@ def Qn2a_iii(staffSalaryList: list):
     for i in range (0, len(staffSalaryList), 1):
         totalSalary += staffSalaryList[i][1]
     averageSalary = totalSalary / len(staffSalaryList)
+    print('List of staffs with salary above average: ')
     for i in range (0, len(staffSalaryList), 1):
         if (staffSalaryList[i][1] > averageSalary):
             print(staffSalaryList[i][0])
 
-def Qn2a_iv(staffSalaryList: list):
+def Qn2a_iv_1(staffSalaryList: list):
     for i in range (0, len(staffSalaryList) - 1, 1):
         indexWithHighestSalary = i
         for n in range(i + 1, len(staffSalaryList), 1):
@@ -65,13 +66,49 @@ def Qn2a_iv(staffSalaryList: list):
             temp = staffSalaryList[i]
             staffSalaryList[i] = staffSalaryList[indexWithHighestSalary]
             staffSalaryList[indexWithHighestSalary] = temp
-            if (i > 0 and staffSalaryList[i][1] == staffSalaryList[i - 1][1]):
-                if (staffSalaryList[i][0] < staffSalaryList[i - 1][0]):
-                    temp = staffSalaryList[i]
-                    staffSalaryList[i] = staffSalaryList[i - 1]
-                    staffSalaryList[i - 1] = temp
+            index = i 
+            while(index >= 1 and    
+                staffSalaryList[index - 1][1] == staffSalaryList[index][1] and
+                staffSalaryList[index - 1][0] > staffSalaryList[index][0] 
+            ):
+                temp = staffSalaryList[index - 1]
+                staffSalaryList[index - 1] = staffSalaryList[index]
+                staffSalaryList[index] = temp
+                index -= 1
+    print('Staff number in descending order of salary: ')
     for i in range (0, len(staffSalaryList), 1):
         print(staffSalaryList[i][0])
+
+def Qn2a_iv_2(staffSalaryList: list):
+    def sortBySalary(item):
+        return item[1]
+    def sortByName(item):
+        return item[0]
+    staffSalaryList.sort(reverse = True, key = sortBySalary)
+    index = 0
+    listOfSalary = []
+    nestedList = []
+    for i in range (0, len(staffSalaryList), 1):
+        if (len(listOfSalary) == 0):
+            listOfSalary.append(staffSalaryList[i][1])
+        elif (staffSalaryList[i][1] != listOfSalary[-1]):
+            listOfSalary.append(staffSalaryList[i][1])
+    index = 0
+    for i in range (0, len(listOfSalary), 1):
+        listOfStaffWithSameSalary = []
+        for n in range(index, len(staffSalaryList), 1):
+            if (staffSalaryList[n][1] == listOfSalary[i]):
+                listOfStaffWithSameSalary.append(staffSalaryList[n])
+            else:
+                index = n
+                break
+        nestedList.append(listOfStaffWithSameSalary)
+    for i in range (0, len(nestedList), 1):
+        if (len(nestedList[i]) > 1):
+            nestedList[i].sort(key = sortByName)
+    for i in range (0, len(nestedList), 1):
+        for n in range(0, len(nestedList[i]), 1):
+            print(nestedList[i][n])
 
 def Qn2b_i(staffSalaryList: list, staffSalaryDict: dict):
     for i in range(0, len(staffSalaryList), 1):
@@ -94,44 +131,31 @@ def Qn2b_iii(staffSalaryDict: dict):
     else:
         print('Unable to locate')
 
-def Qn2b_iv(staffSalaryDict: dict):
-    for key in staffSalaryDict:
-        indexWithHighestSalary = i
-        for n in range(i + 1, len(staffSalaryList), 1):
-            if (staffSalaryList[n][1] > staffSalaryList[indexWithHighestSalary][1]):
-                indexWithHighestSalary = n
-        if (indexWithHighestSalary != i):
-            temp = staffSalaryList[i]
-            staffSalaryList[i] = staffSalaryList[indexWithHighestSalary]
-            staffSalaryList[indexWithHighestSalary] = temp
-            if (i > 0 and staffSalaryList[i][1] == staffSalaryList[i - 1][1]):
-                if (staffSalaryList[i][0] < staffSalaryList[i - 1][0]):
-                    temp = staffSalaryList[i]
-                    staffSalaryList[i] = staffSalaryList[i - 1]
-                    staffSalaryList[i - 1] = temp
-    for i in range (0, len(staffSalaryList), 1):
-        print(staffSalaryList[i][0])
             
 staffSalaryList = [
     ['apple', 1200],
-    ['orange', 600],
     ['zee', 1800],
+    ['aee', 1800],
+    ['orange', 600],
     ['mee', 1800],
-    ['p123', 0]
+    ['p123', 0],
+    ['cee', 1800],
 ]
 staffSalaryDict = {}
-"""
+
 Qn2a_i(staffSalaryList)
 print(staffSalaryList)
 print('\n')
 Qn2a_ii(staffSalaryList)
+print(staffSalaryList)
 print('\n')
 Qn2a_iii(staffSalaryList)
 print('\n')
-Qn2a_iv(staffSalaryList)
+Qn2a_iv_2(staffSalaryList)
 print(staffSalaryList)
 """
 Qn2b_i(staffSalaryList, staffSalaryDict)
-Qn2b_iii(staffSalaryDict)
 Qn2b_ii(staffSalaryDict)
+"""
+
 
